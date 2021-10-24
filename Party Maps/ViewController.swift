213273f7837804696e772ba35fbe4919ac7192d1
Berk Cohadar <berkcohadar1@gmail.com>
2021-10-23 21:33:45 -0400

    Project files uploaded.

---
+++ Party Maps/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Party Maps
+//
+//  Created by Berk Çohadar on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
